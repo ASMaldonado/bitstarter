c4a75307871fd035229e64ce6f3e1d90bf0d1da6
Alan Maldonado <asmmaldonado@gmail.com>
2013-07-14 23:17:20 +0000

    Read file from disc

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var inFile = ("index.html");
+  var fileContent = fs.readFileSync(inFile, "utf8");
+  
+  response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
